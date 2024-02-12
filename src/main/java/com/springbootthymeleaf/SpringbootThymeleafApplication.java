3d3a8cbe25615e5b53e66f643315292a2f99f82f
SAPTARSHI <saptarshi24chakraborty@gmail.com>
2024-02-12 17:53:22 +0530

    new commit

--- src/main/java/com/springbootthymeleaf/SpringbootThymeleafApplication.java
+++ src/main/java/com/springbootthymeleaf/SpringbootThymeleafApplication.java
@@ -8,6 +8,7 @@ public class SpringbootThymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafApplication.class, args);
+		// developed by Saptarshi
 	}
 
 }
